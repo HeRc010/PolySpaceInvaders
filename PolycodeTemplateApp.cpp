a41423b66e4495261f5dcf3a8dc7458e1f147dd4
HeRc010 <adlawren@ualberta.ca>
2014-04-19 00:06:15 +0100

    Added stuffs
    Added some initialization for the fighter

--- PolycodeTemplateApp.cpp
+++ PolycodeTemplateApp.cpp
@@ -1,11 +1,12 @@
 #include "PolycodeTemplateApp.h"
 
+//
+#include "Fighter.h"
+
 PolycodeTemplateApp::PolycodeTemplateApp(PolycodeView *view) {
 	core = new Win32Core(view, 640,480,false, false, 0, 0,60);	  
 	CoreServices::getInstance()->getResourceManager()->addArchive("default.pak");
 	CoreServices::getInstance()->getResourceManager()->addDirResource("default", false);
-
-	// Write your code here
 	
 	// Create screen
 	Screen *main_screen = new Screen();
@@ -13,6 +14,10 @@ PolycodeTemplateApp::PolycodeTemplateApp(PolycodeView *view) {
 	// add background
 	ScreenImage *background = new ScreenImage("Resources/background.png");
 	main_screen->addChild( background );
+
+	// initialize fighter
+	Fighter *player = new Fighter( new ScreenImage("Resources/fighter.png") );
+	main_screen->addChild( player->fighter_sprite );
 }
 PolycodeTemplateApp::~PolycodeTemplateApp() {
     
