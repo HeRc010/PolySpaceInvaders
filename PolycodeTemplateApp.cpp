de4cf2444b64d7a5120e15f913c04b7bf90ca1ce
HeRc010 <adlawren@ualberta.ca>
2014-04-19 00:36:32 +0100

    Modified presentation
    Added GUI parameters, repositioned fighter sprite

--- PolycodeTemplateApp.cpp
+++ PolycodeTemplateApp.cpp
@@ -4,7 +4,16 @@
 #include "Fighter.h"
 
 PolycodeTemplateApp::PolycodeTemplateApp(PolycodeView *view) {
-	core = new Win32Core(view, 640,480,false, false, 0, 0,60);	  
+	// GUI parameters
+	int screen_width = 640;
+	int screen_height = 480;
+
+	float sprite_xscale = 2;
+	float sprite_yscale = 2;
+	Vector3 sprite_scale( sprite_xscale, sprite_yscale, 0 );
+
+	// initialization
+	core = new Win32Core(view, screen_width,screen_height,false, false, 0, 0,60);	  
 	CoreServices::getInstance()->getResourceManager()->addArchive("default.pak");
 	CoreServices::getInstance()->getResourceManager()->addDirResource("default", false);
 	
@@ -16,7 +25,16 @@ PolycodeTemplateApp::PolycodeTemplateApp(PolycodeView *view) {
 	main_screen->addChild( background );
 
 	// initialize fighter
-	Fighter *player = new Fighter( new ScreenImage("Resources/fighter.png") );
+	ScreenImage *player_sprite = new ScreenImage("Resources/fighter.png");
+
+	// adjust accordingly
+	player_sprite->setScale( sprite_scale );
+	
+	float offset = 10;
+	Vector3 location( float( screen_width/2 ), float( screen_height - ( player_sprite->getHeight() * sprite_yscale + offset ) ), 0 );
+	player_sprite->setPosition( location );
+
+	Fighter *player = new Fighter( player_sprite );
 	main_screen->addChild( player->fighter_sprite );
 }
 PolycodeTemplateApp::~PolycodeTemplateApp() {
